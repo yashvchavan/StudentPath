d69c839b0e419fc63052bf0ea5a2ea95ef681834
Yash Chavan <yashvchavan07@gmail.com>
2025-09-17 00:49:52 +0530

    default dark mode changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
-          enableSystem
+          defaultTheme="dark"
+          forcedTheme="dark"
+          enableSystem={false}
           storageKey="studentpath-theme"
           disableTransitionOnChange={false}
         >
